b0f9c4a33933fbbd69c0b6420b5fb8a8c48f0dbc
Raka Wiradinata <rakawiradinata@gmail.com>
2018-05-17 19:39:53 -0400

    updated look + env

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import BusinessList from './components/BusinessList/BusinessList';
-import SearchBar from './components/SearchBar/SearchBar';
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
 
 
 //Object: business (HARDCODE!)
