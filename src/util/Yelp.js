0ecb3d6f02003df34fa92046d46673a3165fbb76
Raka Wiradinata <rakawiradinata@gmail.com>
2018-06-01 23:52:41 -0400

    new additions to code complete

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -6,28 +6,24 @@ const Yelp = {
     const url = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&Location=${location}&sort_by${sortBy}`;
     return fetch(url, {
       headers: {
-        {Authorization: `Bearer ${apiKey}`}
+        Authorization: `Bearer ${apiKey}`
       }
     }).then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error('Request failed!');
-    }, networkError => console.log(networkError.message)
-  ).then(jsonResponse => {
+      return response.json();
+  }).then(jsonResponse => {
     if (jsonResponse.businesses) {
-      return jsonResponse.businesses.map(businesses => {
+      return jsonResponse.businesses.map(business => ({
         id: business.id,
         imageSrc: business.image_url,
         name: business.name,
         address: business.location.address1,
         city: business.location.city,
         state: business.location.state,
-        zipCode: busienss.location.zip_code,
-        category: business.categories,
+        zipCode: business.location.zip_code,
+        category: business.categories[0].title,
         rating: business.rating,
         reviewCount: business.review_count
-      });
+      }));
     }
   });
   }
